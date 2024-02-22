12b15749f70a6a1cf725106e6749e325f6cab929
Duckos-Mods <businwes@hotmail.com>
2024-02-22 03:38:48 +0000

    updated Component Parser to handle things

--- Uranium-234/ComponentParser/ComponentParser.cpp
+++ Uranium-234/ComponentParser/ComponentParser.cpp
@@ -1,6 +1,7 @@
 #include "ComponentParser.hpp"
 #include <fstream>
 #include "../Utils/Logger/Logger.hpp"
+#include "../Utils/Timer.hpp"
 Uranium::ComponentParser::ComponentParser(): m_KeyGen(&this->m_Document) // even though Doc will have no data in it we have to invoke keygen so just wait till after construction
 {
 	std::ifstream file("schemas.json");
@@ -18,10 +19,14 @@ Uranium::ComponentParser::ComponentParser(): m_KeyGen(&this->m_Document) // even
 void Uranium::ComponentParser::Run()
 {
 	Logs::Logger::Info("Locating most up to date keys!");
-	std::array<std::string, 3> highestVersionKeys = m_KeyGen.findHighestVersionKey<3, 1>(this->m_ComponentTypes, this->m_EndData);
+	std::array<KeyInformation, 3> highestVersionKeys = {};
+	{
+		ScopedTimer timer("Finding most up to date keys");
+		highestVersionKeys = m_KeyGen.findHighestVersionKey<3, 1>(this->m_ComponentTypes, this->m_EndData);
+	}
 	Logs::Logger::Info("Found most up to date keys!");
 	for (auto& key : highestVersionKeys) {
-		Logs::Logger::Info("Key: {}", key);
+		Logs::Logger::Info("Highest Version Key: {}\nLongPrefix Key: {}\nShortPrefix Key: {}", key.highestVersionKey, key.longPrefix, key.shortPrefix);
 	}
 	Logs::Logger::Info("Generating component lists!");
 
